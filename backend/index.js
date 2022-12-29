7831a78d4155931d45803a29205bafc9b712f16a
brooklin123 <count2841@gmail.com>
2022-12-29 17:36:52 +0800

    feat: backend

---
+++ backend/index.js
@@ -0,0 +1,27 @@
+import express from "express";
+
+async function startServer() {
+    const app = express();
+    app.use(express.static('public')); //Serves resources from public folder
+    var router = express.Router();
+    app.use('/', router);
+    router.get('/', (req, res) => {
+        console.log('this is root');
+        res.send("this is root"); //direct to homepage
+    })
+
+    app.listen(5001, () => {
+        console.log(`
+            ################################################
+            🛡️  HTTP server listening on port: ${5001} 🛡️
+            ################################################
+        `);
+    })
+        .on("error", (err) => {
+            console.log(err);
+            process.exit(1);
+        });
+
+}
+
+startServer();
