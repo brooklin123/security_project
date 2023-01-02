4da26cf671a4ddbf42aefc99888f49e2c1d3d503
brooklin123 <count2841@gmail.com>
2023-01-02 23:49:38 +0800

    add locustfile

---
+++ locustfile.py
@@ -0,0 +1,20 @@
+import time
+from locust import HttpUser, task, between, events
+# const: 等待固定時間
+# between: 等待在範圍內的隨機時間
+
+class MyUser(HttpUser):
+    wait_time = between(5, 15)
+
+    @task
+    def index(self):
+        self.client.get("/")
+
+
+@events.test_start.add_listener
+def on_test_start(environment, **kwargs):
+    print("A new test is starting")
+
+@events.test_stop.add_listener
+def on_test_stop(environment, **kwargs):
+    print("A new test is ending")
