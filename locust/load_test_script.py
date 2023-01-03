1794f0a0cc31a53b769ab272f4cf3aa5797993c4
brooklin123 <count2841@gmail.com>
2023-01-03 11:18:54 +0800

    feat: distribute locust.py:

--- locust/load_test_script.py
+++ locust/load_test_script.py
@@ -6,8 +6,16 @@ from locust import HttpUser, task, between, events
 class MyUser(HttpUser):
     wait_time = between(5, 15)
 
-    @task
+    @task(2)
     def index(self):
+        with self.client.get("/", catch_response=True) as response:
+            if response.text != "Success":
+                response.failure("Got wrong response")
+            elif response.elapsed.total_seconds() > 0.5:
+                response.failure("Request took too long")
+
+    @task(1)
+    def api(self):
         self.client.get("/api")
 
 
